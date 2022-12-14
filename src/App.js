1c3b9f99d76d48a19300b7a3bb8448a7b44e09b5
gia <gia6shamugia@gmail.com>
2022-12-14 09:36:11 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        SPLI
+        <br />
+        TTER
+      </h1>
+
+      <div class="calc-container">
+        <div class="input-container">
+          <div class="bill">
+            <label>
+              Bill
+              <input type="number" min="1" placeholder="0" />
+            </label>
+          </div>
+
+          <div class="percentage">
+            <p>Select Tip %</p>
+            <button>5%</button>
+            <button>10%</button>
+            <button>15%</button>
+            <button>20%</button>
+            <button>25%</button>
+            <button>50%</button>
+          </div>
+
+          <div class="num-people">
+            <label>
+              Number of People
+              <input type="number" min="10" max="100" />
+            </label>
+          </div>
+        </div>
+
+        <div class="results-container">Will add later</div>
+
+        <div></div>
+      </div>
     </div>
   );
 }
